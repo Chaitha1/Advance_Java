0777c13b0e9cd515012689262f50ec48ef78f8fb
Chaithra1 <R20200581.CHAITHRA@cse.reva.edu.in.com>
2024-02-05 15:59:03 +0530

    First Commit

---
+++ src/com/chai/Arrays/MultidimensionArrays.java
@@ -0,0 +1,18 @@
+package com.chai.Arrays;
+
+public class MultidimensionArrays {
+
+	
+	
+	
+
+	public static void main(String[] args) {
+	 // for the student Name and SRN we need use One-dimension Array
+		
+		
+	
+		
+
+	}
+
+}
